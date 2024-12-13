87af7afeb582cbd0767d42f73658cb85c67c510e
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2024-12-13 19:44:27 +0530

    more oop concept

---
+++ book.java
@@ -0,0 +1,8 @@
+public class book {
+    public String title;
+    public String author;
+
+    public String toString(){
+        return String.format("%s by %s.",this.title, this.author);
+    }
+}
