b3041328c60f7e3d9d390228ade2f21ed2febd99
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2024-12-13 19:01:38 +0530

    sorted an array

--- main.java
+++ main.java
@@ -1,5 +1,18 @@
-class first {
+import java.util.ArrayList;
+import java.util.Comparator;
+
+class Array_sort {
     public static void main(String[] args) {
-        System.out.println("Hello There");
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        numbers.add(50);
+        numbers.add(12);
+        numbers.add(9);
+        numbers.add(144);
+        numbers.add(44);
+
+        numbers.sort(Comparator.naturalOrder());
+
+        System.out.println(numbers.toString());
     }
 }
