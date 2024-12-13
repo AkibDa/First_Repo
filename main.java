07fedc32467adf5e601486f584cdd7484c987da6
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2024-12-13 19:07:00 +0530

    used foreach

--- main.java
+++ main.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 
 class Array_sort {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<Integer> multi = new ArrayList<Integer>();
 
         numbers.add(50);
         numbers.add(12);
@@ -11,7 +11,10 @@ class Array_sort {
         numbers.add(144);
         numbers.add(44);
 
-        numbers.sort(Comparator.naturalOrder());
+        numbers.forEach(number -> {
+            multi.add(number * 2);
+            System.out.println(multi.toString());
+        });
 
         System.out.println(numbers.toString());
     }
