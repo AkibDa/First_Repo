f86be9241f51cb51c15f4bd5bc4beba52c9b0eff
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2024-12-14 18:38:00 +0530

    trying to add springboot

--- book.java
+++
@@ -1,8 +0,0 @@
-public class book {
-    public String title;
-    public String author;
-
-    public String toString(){
-        return String.format("%s by %s.",this.title, this.author);
-    }
-}
